c6906a1d543161f08bcf29835c6cc5a44f0f0344
NiKolayKuksa <nick.k@i.ua>
2017-04-24 14:42:35 +0300

    GUI: basic creteria for import data + grid output

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,24 +3,44 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->resize(QDesktopWidget().availableGeometry(this).size() * 0.7);
+    mainLayout=new QBoxLayout(QBoxLayout::LeftToRight);
+    this->setWindowTitle("Stock. Developed by Mykola Kuksa");
 
-QString sec("OPHC");
-QString from("2017-04-03");
-QString to(2017-04-15);
-QString domain("http://query.yahooapis.com/v1/public/yql?q=");
-QString query(" select * from   yahoo.finance.historicaldata"
-              " where symbol=%22"+sec+"%22 and startDate=%22"
-              +from+"%22 and endDate=%22"+to+"%22");
-QString urltail(" &format=json &diagnostics=true &env=store://datatables.org/alltableswithkeys &callback=");
-QUrl url=QUrl(domain+query+urltail);
+    //==================================================
+    calcPane = new QGroupBox(tr("Calculation"));
+    QHBoxLayout *controlLayout = new QHBoxLayout;
+    //add widgets here
+    calcPane->setLayout(controlLayout);
+    //==================================================
 
-reply=qnam.get(QNetworkRequest(url));
-   connect(reply, SIGNAL(finished()),
-        this, SLOT(httpFinished()));
-   connect(reply, SIGNAL(readyRead()),
-            this, SLOT(httpReadyRead()));
+    //==========Historical data pane===================
+    searchPane = new QGroupBox(tr("Search criteria"));
+    QGridLayout *searchLayout = new QGridLayout;
+    QHBoxLayout *dateSecLayout = new QHBoxLayout;
+    createDateEdit(dateSecLayout);
+    createRadioButton(dateSecLayout);
+    createSearchButton(dateSecLayout,searchPane);
 
+    searchLayout->addLayout(dateSecLayout,0,0,1,1);
+    searchPane->setLayout(searchLayout);
+    historicalDataPane = new QGroupBox(tr("Historical data"));
+    QVBoxLayout *historicalDataLayout = new QVBoxLayout;
+    historicalDataLayout->addWidget(searchPane);
+    table=new QTableWidget();
+    historicalDataLayout->addWidget(table);
+    historicalDataPane->setLayout(historicalDataLayout);
+    //==================================================
+//qDebug()<<fromEdit->date().toString(dateFormat);
+    mainLayout->addWidget(calcPane,1);
+    mainLayout->addWidget(historicalDataPane,1);
 
+    //set layout on window
+    QWidget* wlayout=new QWidget;
+    wlayout->setLayout(mainLayout);
+    this->setCentralWidget(wlayout);
+
+//this->set
 
 }
 
@@ -37,7 +57,7 @@ void MainWindow::httpReadyRead()
 
 void MainWindow::httpFinished()
 {
-    jsonData=reply->readAll();
+    QByteArray jsonData=reply->readAll();
     qDebug()<<jsonData.count();
     //qDebug()<<jsonData;
     QJsonParseError err;
@@ -69,7 +89,7 @@ void MainWindow::httpFinished()
 
         QJsonArray quotes=quote.toArray();
         QJsonObject row;
-        for(size_t i=0;i<quotes.size();i++)
+        /*for(size_t i=0;i<quotes.size();i++)
         {
             row=quotes.at(i).toObject();
             qDebug()<<"Symbol: "<<row.value("Symbol");
@@ -81,14 +101,126 @@ void MainWindow::httpFinished()
             qDebug()<<"Volume: "<<row.value("Volume");
             qDebug()<<"Adj_Close: "<<row.value("Adj_Close");
             qDebug()<<"---------------------------------";
-        }
-       /* if (!qur.isString()) {
-            qDebug() << "Value not string";
-            //return;
         }*/
 
-        //qDebug() << jsonValue.toString();
+       table->setColumnCount(8);
+       QStringList headers;
+       headers<<"Symbol"<<"Date"<<"Open"<<"High"<<"Low"<<"Close"<<"Volume"<<"Adj_Close";
+       table->setHorizontalHeaderLabels(headers);
+       for(int i=0;i<quotes.size();i++){
+            table->insertRow(i);
+            QTableWidgetItem* celli0=new QTableWidgetItem();
+            QTableWidgetItem* celli1=new QTableWidgetItem();
+            QTableWidgetItem* celli2=new QTableWidgetItem();
+            QTableWidgetItem* celli3=new QTableWidgetItem();
+            QTableWidgetItem* celli4=new QTableWidgetItem();
+            QTableWidgetItem* celli5=new QTableWidgetItem();
+            QTableWidgetItem* celli6=new QTableWidgetItem();
+            QTableWidgetItem* celli7=new QTableWidgetItem();
 
+            row=quotes.at(i).toObject();
+            celli0->setText(row.value("Symbol").toString());
+            celli1->setText(row.value("Date").toString());
+            celli2->setText(row.value("Open").toString());
+            celli3->setText(row.value("High").toString());
+            celli4->setText(row.value("Low").toString());
+            celli5->setText(row.value("Close").toString());
+            celli6->setText(row.value("Volume").toString());
+            celli7->setText(row.value("Adj_Close").toString());
+
+            table->setItem(i,0,celli0);
+            table->setItem(i,1,celli1);
+            table->setItem(i,2,celli2);
+            table->setItem(i,3,celli3);
+            table->setItem(i,4,celli4);
+            table->setItem(i,5,celli5);
+            table->setItem(i,6,celli6);
+            table->setItem(i,7,celli7);
+        }
+
+}
+
+void MainWindow::createDateEdit(QHBoxLayout *layout)
+{
+    QString dateFormat("yyyy-MM-dd");
+
+    QGroupBox *fromToDatePane = new QGroupBox;
+    QHBoxLayout *fromToDateLayout = new QHBoxLayout;
+    fromToDatePane->setLayout(fromToDateLayout);
+
+    QFormLayout *fromLayout=new QFormLayout;
+    QLabel *fromLabel =new QLabel("From",fromToDatePane);
+    fromDateEdit = new QDateEdit(QDate::currentDate(),fromToDatePane);
+    fromLayout->addRow(fromLabel,fromDateEdit);
+    fromDateEdit->setDisplayFormat(dateFormat);
+
+    QFormLayout *toLayout=new QFormLayout;
+    QLabel *toLabel = new QLabel("To",fromToDatePane);
+    toDateEdit = new QDateEdit(QDate::currentDate(),toLabel);
+    toLayout->addRow(toLabel,toDateEdit);
+    toDateEdit->setDisplayFormat(dateFormat);
+
+    //dateEdit->setMinimumDate(QDate::currentDate().addDays(-365));
+    //dateEdit->setMaximumDate(QDate::currentDate().addDays(365));
+    fromToDateLayout->addLayout(fromLayout);
+    fromToDateLayout->addLayout(toLayout);
+
+    layout->addWidget(fromToDatePane);
+}
+
+void MainWindow::createRadioButton(QHBoxLayout *layout)
+{
+    QGroupBox *radioPane = new QGroupBox;
+    QHBoxLayout *radioLayout = new QHBoxLayout;
+    QRadioButton *shareRadioButton = new QRadioButton(tr("&Share"));
+    QRadioButton *indexRadioButton = new QRadioButton(tr("Market index"));
+    radioLayout->addWidget(shareRadioButton);
+    radioLayout->addWidget(indexRadioButton);
+    radioPane->setLayout(radioLayout);
+    layout->addWidget(radioPane);
+
+    shareRadioButton->setChecked(true);
+    secType=security;
+    connect(shareRadioButton, SIGNAL(clicked(bool)),
+             this, SLOT(shareClicked(bool)));
+    connect(indexRadioButton, SIGNAL(clicked(bool)),
+             this, SLOT(indexClicked(bool)));
+}
+
+void MainWindow::createSearchButton(QHBoxLayout *layout, QWidget *parent)
+{
+    QPushButton *searchButton=new QPushButton("search",parent);
+    layout->addWidget(searchButton);
+    connect(searchButton, SIGNAL(clicked()), this, SLOT(httpRequest()));
+}
+
+void MainWindow::shareClicked(bool is)
+{
+    secType=security;
+}
+
+void MainWindow::indexClicked(bool is)
+{
+    secType=index;
+}
+
+void MainWindow::httpRequest()
+{
+    QString dateFormat("yyyy-MM-dd");
+
+    QString sec("OPHC");
+    QString from=fromDateEdit->date().toString(dateFormat);
+    QString to=toDateEdit->date().toString(dateFormat);
+    QString domain("http://query.yahooapis.com/v1/public/yql?q=");
+    QString query(" select * from   yahoo.finance.historicaldata"
+                 " where symbol=%22"+sec+"%22 and startDate=%22"
+                 +from+"%22 and endDate=%22"+to+"%22");
+    QString urltail(" &format=json &diagnostics=true &env=store://datatables.org/alltableswithkeys &callback=");
+    QUrl url=QUrl(domain+query+urltail);
+
+    reply=qnam.get(QNetworkRequest(url));
+    connect(reply, SIGNAL(finished()), this, SLOT(httpFinished()));
+    connect(reply, SIGNAL(readyRead()), this, SLOT(httpReadyRead()));
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -13,6 +13,17 @@
 #include <QJsonDocument>
 #include <QJsonParseError>
 #include <QJsonArray>
+#include <QDesktopWidget>
+#include <QBoxLayout>
+#include <QTableWidget>
+#include <QGroupBox>
+#include <QDateEdit>
+#include <QLabel>
+#include <QRadioButton>
+#include <QFormLayout>
+#include <QPushButton>
+
+enum SecurityType{security, index};
 
 class MainWindow : public QMainWindow
 {
@@ -22,11 +33,27 @@ private slots:
     //void cancelDownload();
     void httpFinished();
     void httpReadyRead();
+    void httpRequest();
+    void shareClicked(bool is);
+    void indexClicked(bool is);
+
 
 private:
     QNetworkAccessManager qnam;
     QNetworkReply *reply;
-    QByteArray jsonData;
+   //Gui
+    QGroupBox *calcPane;
+    QGroupBox *searchPane;
+    QGroupBox *historicalDataPane;
+    QBoxLayout* mainLayout;
+    QTableWidget* table;
+    QDateEdit *fromDateEdit;
+    QDateEdit *toDateEdit;
+    SecurityType secType;
+
+    void createRadioButton(QHBoxLayout *layout);
+    void createDateEdit(QHBoxLayout *layout);
+    void createSearchButton(QHBoxLayout *layout,QWidget *parent = 0);
 
 public:
     MainWindow(QWidget *parent = 0);
